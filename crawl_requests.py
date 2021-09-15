cde8baec793e8bb86bb47cb90994311c3f814c6f
hyeonski <hyeonski@student.42seoul.kr>
2021-09-16 00:16:04 +0900

    [Etc]: database 작업 중

--- crawl_requests.py
+++ crawl_requests.py
@@ -1,3 +1,4 @@
+from database import AttachedFile, Post, PostType
 import requests
 from bs4 import BeautifulSoup
 import base64
@@ -6,7 +7,9 @@ import datetime
 import sys
 from urllib.parse import unquote
 from multiprocessing import Process
+from database import Post
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
 
 def printErrorEndl(error):
     sys.stderr.write(f"[{datetime.datetime.now()}] Error: {error}\n")
@@ -42,13 +45,11 @@ def getFormDataFromInputs(soup, formData):
         value = input.get('value')
         formData[input.get('name')] = value if value != None else ''
 
-def crawlBoard(boardUrl):
+def crawlBoard(boardUrl, postTypeId, dbSession):
     # Get Session
     cookies = dict(requests.get('https://seo2.sen.es.kr/', allow_redirects=False).cookies)
     userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36'
 
-
-    count = 0
     formData = {
         'bbsId': '',
         'bbsTyCode': '',
@@ -66,9 +67,10 @@ def crawlBoard(boardUrl):
         for post in posts:
             if len(post.find_all('td')) == 1:
                 return
+            if formData['pageIndex'] != 1 and post.td.span != None and post.td.span.text == '공지':
+                continue
 
-            print(count)
-            count += 1
+
             formData['nttId'] = post.select_one('td > a').get('onclick').replace(f"fnView('{formData['bbsId']}', '", '').replace("');", '') # onclick 함수의 인자에서 nttId 값 추출
             formData['ifNttId'] = base64.b64encode(formData['nttId'].encode('ascii')).decode('ascii')
             postResponse = myPost('https://seo2.sen.es.kr/dggb/module/board/selectBoardDetailAjax.do', cookies=cookies, data=formData)
@@ -76,12 +78,16 @@ def crawlBoard(boardUrl):
 
             elemsIntable = soup.select('tbody > tr > td > div')
 
-            author = elemsIntable[0].text.strip()
-            upload_at = elemsIntable[1].text.strip()
-            title = elemsIntable[2].text.strip()
-            content = elemsIntable[3]
-            print(author, upload_at, title)
+            post = Post(
+                id=int(formData['nttId']),
+                type=postTypeId,
+                author=elemsIntable[0].text.strip(),
+                upload_at=datetime.datetime.strptime(elemsIntable[1].text.strip(), '%Y-%m-%d'),
+                title=elemsIntable[2].text.strip(),
+                content=str(elemsIntable[3])
+                )
 
+            attachedFiles = []
             fileDiv = soup.select_one('div#file_div')
             if fileDiv != None:
                 atchFileId = fileDiv.select_one('input[name=atchFileId]').get('value')
@@ -98,23 +104,48 @@ def crawlBoard(boardUrl):
                     fileDownCnt += 1
                     fileName = unquote(fileResponse.headers['Content-Disposition'].split('filename=')[1])
                     fileSize = fileResponse.headers['Content-Length']
-                    print(fileName, fileSize)
-            print("\n")
+                    attachedFiles.append(AttachedFile(
+                        post=post.id,
+                        attachedFileId=atchFileId,
+                        fileSn=fileSn,
+                        name=fileName,
+                        size=fileSize,
+                        downloadUrl=fileUrl,
+                        previewUrl=f'http://viewhosting.ssem.or.kr:8080/SynapDocViewServer/job?fid={atchFileId}_{fileSn}&filePath=https://seo2.sen.es.kr:443/dggb/cnvrFileDown.do?atchFileId={atchFileId}:{fileSn}&convertType=0&fileType=URL&sync=true'
+                    ))
+            post.attachedFiles = attachedFiles
+            #####
+            try:
+                dbSession.add(post)
+                dbSession.commit()
+            except Exception as e:
+                if e.args[0] == PY:
+                    dbSession.rollback()
+                    dbSession.flush()
+                printErrorEndl(f'[{datetime.datetime.now()}] d{e.args[0]}d')
+            #####
 
         formData['nttId'] = ''
         formData['ifNttId'] = ''
         formData['pageIndex'] = str(int(formData['pageIndex']) + 1)
 
+def worker(boardUrl, postTypeName):
+    engine = create_engine('mysql+pymysql://admin:12345678@elementary-crawl.c6pvxgc9cmjq.ap-northeast-2.rds.amazonaws.com/elementary')
+    dbSession = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+    postType = dbSession.query(PostType).filter(PostType.name == postTypeName).one()
+    crawlBoard(boardUrl, postType.id, dbSession)
+
+
 if __name__ == '__main__':
-    p1 = Process(target=crawlBoard, args=('https://seo2.sen.es.kr/78584/subMenu.do',))
-    p2 = Process(target=crawlBoard, args=('https://seo2.sen.es.kr/78585/subMenu.do',))
-    p3 = Process(target=crawlBoard, args=('https://seo2.sen.es.kr/113876/subMenu.do',))
+    # p1 = Process(target=crawlBoard, args=('https://seo2.sen.es.kr/78584/subMenu.do',))
+    # p2 = Process(target=crawlBoard, args=('https://seo2.sen.es.kr/78585/subMenu.do',))
+    p3 = Process(target=worker, args=('https://seo2.sen.es.kr/113876/subMenu.do', 'school_meal'))
     
-    p1.start()
-    p2.start()
+    # p1.start()
+    # p2.start()
     p3.start()
 
-    p1.join()
-    p2.join()
+    # p1.join()
+    # p2.join()
     p3.join()
-    # engine = create_engine('mysql://admin:12345678@elementary-crawl.c6pvxgc9cmjq.ap-northeast-2.rds.amazonaws.com/elementary')
+    
