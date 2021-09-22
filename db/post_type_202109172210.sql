772e8fe32c20fac546b2327094853ee1bd703431
hyeonski <hyeonski@student.42seoul.kr>
2021-09-22 21:43:41 +0900

    [Etc]: db migration

--- db/post_type_202109172210.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO elementary.post_type (name) VALUES
-	 ('notice'),
-	 ('parent_letter'),
-	 ('school_meal');
---
+++ db/post_type_20210922.sql
@@ -0,0 +1,4 @@
+INSERT INTO elementary.post_type (name) VALUES
+	 ('notice'),
+	 ('parent_letter'),
+	 ('school_meal');
