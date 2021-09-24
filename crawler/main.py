d1b162ca3bde6de8ce9a7037056b0b6e16798be2
hyeonski <hyeonski@student.42seoul.kr>
2021-09-24 22:37:36 +0900

    [Refactor]: Changed Data Store Method to pymysql
    Delete ORM, changed to pymysql raw query

--- crawler/main.py
+++ crawler/main.py
@@ -4,13 +4,11 @@ import base64
 import datetime
 from urllib.parse import unquote
 from multiprocessing import Process
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
-from sqlalchemy.exc import IntegrityError
-from database import AttachedFile, Post, PostType
 from utils import myGet, myPost, printErrorEndl
+import pymysql
 
 PYMYSQL_DUPLICATE_ERROR = 1062
+
 
 def getFormDataFromInputs(soup, formData):
     inputs = soup.select('input[type=hidden]')
@@ -18,7 +16,8 @@ def getFormDataFromInputs(soup, formData):
         value = input.get('value')
         formData[input.get('name')] = value if value != None else ''
 
-def crawlBoard(boardUrl, postTypeId, dbSession):
+
+def crawlBoard(boardUrl, postTypeId, dbConnection):
     # Get Session
     cookies = dict(requests.get('https://seo2.sen.es.kr/', allow_redirects=False).cookies)
     userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36'
@@ -31,8 +30,9 @@ def crawlBoard(boardUrl, postTypeId, dbSession):
     }
     getFormDataFromInputs(BeautifulSoup(myGet(boardUrl, cookies=cookies).text, 'html.parser'), formData)
 
-    formData['customRecordCountPerPage'] = 1000 # 한 페이지 당 게시글 수 조정
+    formData['customRecordCountPerPage'] = 1000 # 리스트 한 페이지 당 게시글 수 조정
     while True:
+        # Get Post List
         listResponse = myPost('https://seo2.sen.es.kr/dggb/module/board/selectBoardListAjax.do', cookies=cookies, data=formData)
         soup = BeautifulSoup(listResponse.text, 'html.parser')
 
@@ -40,10 +40,11 @@ def crawlBoard(boardUrl, postTypeId, dbSession):
         for post in posts:
             if len(post.find_all('td')) == 1:
                 return
+            # 첫 페이지 제외 공지는 건너뛰도록
             if formData['pageIndex'] != 1 and post.td.span != None and post.td.span.text == '공지':
                 continue
 
-
+            # Get Post Detail
             formData['nttId'] = post.select_one('td > a').get('onclick').replace(f"fnView('{formData['bbsId']}', '", '').replace("');", '') # onclick 함수의 인자에서 nttId 값 추출
             formData['ifNttId'] = base64.b64encode(formData['nttId'].encode('ascii')).decode('ascii')
             postResponse = myPost('https://seo2.sen.es.kr/dggb/module/board/selectBoardDetailAjax.do', cookies=cookies, data=formData)
@@ -51,16 +52,23 @@ def crawlBoard(boardUrl, postTypeId, dbSession):
 
             elemsIntable = soup.select('tbody > tr > td > div')
 
-            post = Post(
-                id=int(formData['nttId']),
-                type=postTypeId,
-                author=elemsIntable[0].text.strip(),
-                upload_at=datetime.datetime.strptime(elemsIntable[1].text.strip(), '%Y-%m-%d'),
-                title=elemsIntable[2].text.strip(),
-                content=str(elemsIntable[3]).lstrip('<div class="content">').rstrip('</div>'),
-                )
+            postId = formData['nttId']
+            author = elemsIntable[0].text.strip()
+            uploadAt = elemsIntable[1].text.strip()
+            title = dbConnection.escape_string(elemsIntable[2].text.strip())
+            content = dbConnection.escape_string(str(elemsIntable[3]).lstrip('<div class="content">').rstrip('</div>'))
+            print(f'{postId} {author} {uploadAt} {title}')
+            
+            # Insert Post or Update Post
+            cursor = dbConnection.cursor()
+            cursor.execute(f"SELECT id FROM post WHERE id='{postId}'")
+            if cursor.fetchone() == None:
+                cursor.execute(f"INSERT INTO post (id, post_type_id, author, upload_at, title, content) VALUES ('{postId}', '{postTypeId}', '{author}', '{uploadAt}', '{title}', '{content}')")
+            else:
+                cursor.execute(f"UPDATE post SET author='{author}', upload_at='{uploadAt}', title='{title}', content='{content}' WHERE id='{postId}'")
+            dbConnection.commit()
 
-            attachedFiles = []
+            # Get Attached Files
             fileDiv = soup.select_one('div#file_div')
             if fileDiv != None:
                 atchFileId = fileDiv.select_one('input[name=atchFileId]').get('value')
@@ -75,42 +83,42 @@ def crawlBoard(boardUrl, postTypeId, dbSession):
                     if 'Content-Disposition' not in fileResponse.headers:
                         continue
                     fileDownCnt += 1
-                    fileName = unquote(fileResponse.headers['Content-Disposition'].split('filename=')[1])
+                    fileName = dbConnection.escape_string(unquote(fileResponse.headers['Content-Disposition'].split('filename=')[1]))
                     fileSize = fileResponse.headers['Content-Length']
-                    attachedFiles.append(AttachedFile(
-                        post=post.id,
-                        attachedFileId=atchFileId,
-                        fileSn=fileSn,
-                        name=fileName,
-                        size=fileSize,
-                    ))
-            post.attachedFiles = attachedFiles
 
-            try:
-                dbSession.add(post)
-                dbSession.commit()
-            except IntegrityError as e:
-                dbSession.rollback()
-                printErrorEndl(e.args[0])
-                if e.orig.args[0] != PYMYSQL_DUPLICATE_ERROR:
-                    raise e
+                    # Insert or Update File
+                    cursor = dbConnection.cursor()
+                    cursor.execute(f"SELECT id FROM attached_file WHERE attached_file_id='{atchFileId}' AND file_sn='{fileSn}'")
+                    if cursor.fetchone() == None:
+                        cursor.execute(f"INSERT INTO attached_file (post_id, attached_file_id, file_sn, name, size) VALUES ('{postId}', '{atchFileId}', '{fileSn}', '{fileName}', '{fileSize}')")
+                    else:
+                        cursor.execute(f"UPDATE attached_file SET name='{fileName}', size='{fileSize}' WHERE attached_file_id='{atchFileId}' AND file_sn='{fileSn}'")
+                    dbConnection.commit()
 
         formData['nttId'] = ''
         formData['ifNttId'] = ''
         formData['pageIndex'] = str(int(formData['pageIndex']) + 1)
 
+
+
 def worker(boardUrl, postTypeName):
-    engine = create_engine('mysql+pymysql://root:1234@localhost/elementary')
-    dbSession = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
-    postType = dbSession.query(PostType).filter(PostType.name == postTypeName).one()
-    crawlBoard(boardUrl, postType.id, dbSession)
+    dbConnection = pymysql.connect(host='localhost', user='root', password='1234', db='elementary', charset='utf8')
+    cursor = dbConnection.cursor()
+    cursor.execute(f"SELECT id FROM post_type WHERE name='{postTypeName}'")
+    postTypeId = cursor.fetchone()[0]
+
+    crawlBoard(boardUrl, postTypeId, dbConnection)
+    dbConnection.close()
 
 
 if __name__ == '__main__':
-    p1 = Process(target=worker, args=('https://seo2.sen.es.kr/78584/subMenu.do', 'notice'))
-    p2 = Process(target=worker, args=('https://seo2.sen.es.kr/78585/subMenu.do', 'parent_letter'))
-    p3 = Process(target=worker, args=('https://seo2.sen.es.kr/113876/subMenu.do', 'school_meal'))
-    
+    p1 = Process(target=worker, args=(
+        'https://seo2.sen.es.kr/78584/subMenu.do', 'notice'))
+    p2 = Process(target=worker, args=(
+        'https://seo2.sen.es.kr/78585/subMenu.do', 'parent_letter'))
+    p3 = Process(target=worker, args=(
+        'https://seo2.sen.es.kr/113876/subMenu.do', 'school_meal'))
+
     p1.start()
     p2.start()
     p3.start()
@@ -118,4 +126,3 @@ if __name__ == '__main__':
     p1.join()
     p2.join()
     p3.join()
-    
