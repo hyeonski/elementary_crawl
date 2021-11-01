1f9756a894ce9583535b7d7eb000f964ada003e4
hyeonski <hyeonski@student.42seoul.kr>
2021-11-01 17:11:41 +0900

    [Etc]: Delete thread pool

--- crawler/src/main.py
+++ crawler/src/main.py
@@ -1,5 +1,4 @@
 from typing import List
-from threading import Thread
 
 from dotenv import load_dotenv
 from crawler.crawler import ACrawler
@@ -7,9 +6,6 @@ from crawler.crawler import ACrawler
 from crawler.school.seoul_seoi import SeoulSeoiCrawler
 from crawler.school.jeonju_inbong import JeonjuInbongCrawler
 from crawler.school_meal import SchoolMealCrawler
-
-def worker(crawler: ACrawler):
-    crawler.run()
 
 
 def main():
@@ -19,11 +15,8 @@ def main():
         SeoulSeoiCrawler('서울서이초등학교', 'https://seo2.sen.es.kr'),
         JeonjuInbongCrawler('전주인봉초등학교', 'https://school.jbedu.kr/inbong'),
     ]
-    threads = [Thread(target=worker, args=(crawler,)) for crawler in crawlers]
-    for thread in threads:
-        thread.start()
-    for thread in threads:
-        thread.join()
+    for crawler in crawlers:
+        crawler.run()
 
 
 if __name__ == "__main__":
